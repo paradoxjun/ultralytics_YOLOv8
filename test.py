7e52a8234980371b42c6ba2bbb0da2f3278ab58c
chenjun <chenjun97@stu.xmu.edu.cn>
2024-05-10 14:58:01 +0800

    测试脚本

--- test.py
+++ test.py
@@ -1,2 +1,15 @@
-import ultralytics
-ultralytics.checks()
+from ultralytics import YOLO
+
+data_path = r"/home/chenjun/code/datasets/bank_monitor/data_05_09"
+# Load a model
+model = YOLO(model='./runs/detect/train_05_09_01/weights/best.pt', task="detect")  # pretrained YOLOv8n model
+
+# Run batched inference on a list of images
+results = model(data_path, stream=True)  # return a generator of Results objects
+
+# Process results generator
+for result in results:
+    boxes = dir(result)  # Boxes object for bounding box outputs
+    print(result.boxes.cls)
+    print("*" * 20)
+    # result.save(filename='result.jpg')  # save to disk
