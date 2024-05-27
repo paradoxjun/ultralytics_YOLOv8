8c3ce7fa7bfeb1addbbcaf42f36997eff6b93e3c
chenjun <chenjun97@stu.xmu.edu.cn>
2024-05-27 10:48:47 +0800

    封装VideoTracker类实现deep_sort追踪

--- ultralytics/task_bank/utils.py
+++ ultralytics/task_bank/utils.py
@@ -1,7 +1,6 @@
-import numpy as np
-import cv2
 import os
 import yaml
+import torch
 from easydict import EasyDict
 
 palette = (2 ** 11 - 1, 2 ** 15 - 1, 2 ** 20 - 1)
@@ -13,25 +12,6 @@ def compute_color_for_labels(label):
     """
     color = [int((p * (label ** 2 - label + 1)) % 255) for p in palette]
     return tuple(color)
-
-
-def draw_boxes(img, bbox, cls, identities=None, offset=(0, 0)):
-    print(f"id: {identities}")
-    for i, box in enumerate(bbox):
-        x1, y1, x2, y2 = [int(i) for i in box]
-        x1 += offset[0]
-        x2 += offset[0]
-        y1 += offset[1]
-        y2 += offset[1]
-        # box text and bar
-        id = int(identities[i]) if identities is not None else 0
-        color = compute_color_for_labels(id)
-        label = '{}{:d}'.format("", id)
-        t_size = cv2.getTextSize(label, cv2.FONT_HERSHEY_PLAIN, 2, 2)[0]
-        cv2.rectangle(img, (x1, y1), (x2, y2), color, 3)
-        cv2.rectangle(img, (x1, y1), (x1 + t_size[0] + 3, y1 + t_size[1] + 4), color, -1)
-        cv2.putText(img, label, (x1, y1 + t_size[1] + 4), cv2.FONT_HERSHEY_PLAIN, 2, [255, 255, 255], 2)
-    return img
 
 
 class YamlParser(EasyDict):
@@ -56,6 +36,43 @@ class YamlParser(EasyDict):
 
 def get_config(config_file=None):
     return YamlParser(config_file=config_file)
+
+
+def transform_and_concat_tensors(tensor_list, k1_v1_dict_list, k2_v2_dict):
+    """
+    >>> tensor_list = [p_1.boxes.cls, p_2.boxes.cls]
+    >>> k1_v2_dict_list = [p_1.names, p_2.names]
+    >>> res = transform_and_concat_tensors(tensor_list, k1_v2_dict_list, class_name_num_str)
+    Args:
+        tensor_list: 标签张量列表
+        k1_v1_dict_list: 原模型标签字典
+        k2_v2_dict: 修改后的标签字典
+    Returns: 修改后的标签张量
+    """
+    def transform_tensor(tensor, k1_v1_dict):
+        original_dtype = tensor.dtype       # 获取输入tensor的数据类型
+        original_device = tensor.device     # 获取输入tensor的device
+        v1_to_k2 = {v: k for k, v in k2_v2_dict.items()}    # 创建 v1 到 k2 的映射字典
+        transformed_list = []               # 创建一个新的列表来存储转换后的值
+
+        for value in tensor:
+            v1 = k1_v1_dict[value.item()]   # 获取原模型的
+
+            if v1 not in v1_to_k2:          # 合并后的类别标签不存在
+                raise ValueError(f"label [{v1}] from model not found in new labels.")
+
+            k2 = v1_to_k2[v1]
+            transformed_list.append(k2)
+
+        transformed_tensor = torch.tensor(transformed_list, dtype=original_dtype, device=original_device)
+        return transformed_tensor
+
+    transformed_tensors = [transform_tensor(tensor, k1_v1_dict) for tensor, k1_v1_dict in
+                           zip(tensor_list, k1_v1_dict_list)]   # 对每个 tensor 进行转换
+
+    result_tensor = torch.cat(transformed_tensors, dim=0)       # 将转换后的 tensor 按 dim=1 进行连接
+
+    return result_tensor
 
 
 if __name__ == '__main__':
